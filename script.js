dfd21c03c5c1f3957655e1459191bd7152177b08
FARES MOHAMED <119041871+faresmohamed0074@users.noreply.github.com>
2023-10-06 07:56:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let table = document.getElementsByClassName("sheet-body")[0],
+rows = document.getElementsByClassName("rows")[0],
+columns = document.getElementsByClassName("columns")[0]
+tableExists = false
+
+const generateTable = () => {
+  let rowsNumber = parseInt(rows.value),
+    columnsNumber = parseInt(columns.value);
+
+  if (isNaN(rowsNumber) || isNaN(columnsNumber)) {
+    Swal.fire({
+      icon: "error",
+      title: "Empty Fields",
+      text: "Please enter the number of rows and columns.",
+    });
+    return;
+  }
+
+  table.innerHTML = "";
+
+  for (let i = 0; i < rowsNumber; i++) {
+    var tableRow = "";
+    for (let j = 0; j < columnsNumber; j++) {
+      tableRow += `<td contenteditable></td>`;
+    }
+    table.innerHTML += tableRow;
+  }
+
+  if (rowsNumber > 0 && columnsNumber > 0) {
+    tableExists = true;
+  }
+};
+
+const ExportToExcel = (type, fn, dl) => {
+  if (!tableExists) {
+    Swal.fire({
+      icon: "error",
+      title: "No Table Found",
+      text: "Please generate a table before exporting.",
+    });
+    return;
+  }
+
+  var elt = table;
+  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+  return dl
+    ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+    : XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+};
